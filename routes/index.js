40e5d018d03a4d0ffeccb0452d92e8d8d9baa49d
Amy Liu <amyjialiu2015@gmail.com>
2015-08-12 10:11:23 -0700

    save and upload json file/local finished

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Tree structure', { title: 'Express' });
+  res.render('newtree', { title: 'Express' });
 });
 
 router.get('/tree',function(req,res){
   res.render('Tree structure');
 });
 
+router.get('/newtree',function(req,res){
+  res.render('newtree');
+});
 module.exports = router;
